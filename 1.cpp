5107df9ddfde3333b1a2a258aeab5671771395a8
Kaustav Saha <64833209+kaustavsaha018@users.noreply.github.com>
2020-09-07 21:45:31 +0530

    By Kaustav Saha
    Updated

--- 1.cpp
+++ 1.cpp
@@ -20,11 +20,19 @@ int item::count;
 int main(){
 	
 	item n1,n2,n3;
+	
+	n1.getCount();
+	n2.getCount();
+	n3.getCount();
+	
 	n1.getNumber(1);
 	n1.getCount();
+	
 	n2.getNumber(2);
 	n2.getCount();
+	
 	n3.getNumber(3);
 	n3.getCount();
+	
 	return 0;
 }
